1c056a65bec6ea1bec6bf0d59dce57353da8bed8
FrancescoGalanti <galanti_francesco>
2020-10-19 15:08:43 +0200

    working

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,47 +4,18 @@
 
 
 
-// // prima variabile //
-//  var email = ["email@francesco.it", "email@dario.it", "email@laura.it"];
-//
-//  // seconda variabile //
-//
-//  var emailuser = prompt("Inserisci la tua email").toLowerCase();
-//
-//  // controllo//
-//
-//  var userfound = false;
-//
-//
-//  for( var i = 0;  i < email.length; i++ ){
-//
-//     var item = email[i];
-//
-//     if( item === emailuser) {
-//         userfound = true;
-//     }
-//
-//  }
-//
-//  if(userfound == true) {
-//    console.log("Utente trovato, procedi con l\"accesso");
-//  } else(
-//     console.log("errore di autenticazione")
-//  )
+// prima variabile //
+ var array = ['bianchi', 'rossi', 'duzioni', 'balsano', 'giorgini'];
 
-// // secondo esercizio //
-//
-// // prima variabile //
-// var numerogiocatore = Math.floor((Math.random() * 6) + 1);
-//
-// // console.log(numerogiocatore) //
-//
-// var numerocomputer = Math.floor((Math.random() * 6) + 1);
-//
-// // output //
-//
-// if(numerogiocatore > numerocomputer) {
-//   console.log("IL GIOCATORE VINCE")
-// } else{
-//   console.log("IL COMPUTER VINCE")
-// }
+
+// Push nome utente //
+
+array.push(prompt("Quale il tuo cognome ?"));
+
+console.log(array);
+
+// alphabetic order //
+
+array.sort(function(a,b);
+
+console.log(array.sort);
